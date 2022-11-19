890dd3bc36161d257210c7b126b7d96f23c137ba
lucascant <lucas.cantoaria@gmail.com>
2022-11-19 10:27:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ In the meantime, below is an example of what you can do with just a few lines of
 st.title('Uber pickups in NYC')
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    total_points = st.slider("Number of points in spiral LLL", 1, 5000, 1)
+    num_turns = st.slider("Number of turns in spiral LLL", 1, 100, 1)
 
     Point = namedtuple('Point', 'x y')
     data = []
